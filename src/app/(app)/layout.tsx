9313037df95c6e2863ed215a38e80b26553c66be
Martin Elmars <121293732+elmars-hub@users.noreply.github.com>
2026-03-15 21:01:58 +0100

    Remove comment from app layout

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -4,7 +4,6 @@ import { DesktopSidebar } from "@/components/desktop-sidebar";
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-dvh overflow-hidden bg-background">
-      {/* sidebar */}
       <DesktopSidebar />
       <div className="flex w-full flex-1 flex-col overflow-hidden lg:w-auto">
         <AppHeader />
